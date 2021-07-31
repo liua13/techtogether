bb8a6b38e4a32b393fd65591b80ca298a21a976b
liua13 <liua1@bxscience.edu>
2021-07-31 11:12:58 -0400

    fix nav + faq

--- src/components/faq.js
+++ src/components/faq.js
@@ -5,18 +5,36 @@ import faqData from "../data/faq.json";
 const Faq = () => {
     return (
         <section id="faq">
-            <div className="faqSubcontainer">
+            <div className="faqContainer">
                 <h2>No Dumb Questions</h2>
 
-                <dl>
-                    {faqData.map((data) => (
-                        <Dropdown
-                            header={data.question}
-                            body={data.answer}
-                            key={data.question}
-                        />
-                    ))}
-                </dl>
+                <div className="faqSubcontainer">
+                    <div className="faqHalfContainer">
+                        {faqData.map((data, index) => {
+                            if (index % 2 == 0) {
+                                return (
+                                <Dropdown
+                                    header={data.question}
+                                    body={data.answer}
+                                    key={data.question}
+                                />)
+                            }
+                        })}
+                    </div>
+                    
+                    <div className="faqHalfContainer" style={{verticalAlign: "top"}}>
+                        {faqData.map((data, index) => {
+                            if (index % 2 == 1) {
+                                return (
+                                <Dropdown
+                                    header={data.question}
+                                    body={data.answer}
+                                    key={data.question}
+                                />)
+                            }
+                        })}
+                    </div>
+                </div>
             </div>
         </section>
     )
